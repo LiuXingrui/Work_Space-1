82284e30b5f116334f385b5a0459309dd176d20e
LiuXingrui <liuxingrui1749@gmail.com>
2020-08-12 12:26:13 -0700

    add random generator function, need to install ntl by myself. makefile update:g++ -std=c++11

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 load_all: load.sh
 	source load.sh
+g+++: circulant_QHP.cpp
+	g++ -std=c++11 -o circulant_QHP.out circulant_QHP.cpp
--- circulant_QHP.cpp
+++ circulant_QHP.cpp
@@ -1,36 +1,86 @@
-#include <NTL/ZZ.h> 
-#include <NTL/vector.h>
+ 
+
 #include <iostream>
 #include<cstring>
 #include<string>
 #include<vector>
 #include <fstream>
-#include <NTL/ZZXFactoring.h>
-#include <NTL/ZZ_pXFactoring.h>
-#include <NTL/ZZ_pEX.h>
+#include<random>
+#include <chrono>
 
 
-using std::default_random_engine generator; 
-using std::uniform_int_distribution<int>;
+
 using std::cin;
 using std::cout; 
 using std::vector;
-using namespace NTL;
-vector<unsigned> rand_vec (const int n){
-  static default_random_engine e;
-  static uniform_int_distribution<int> u(0,1);
-  vector<int> ret;
-  for(size_t i=0;i<n;++i){
-    ret.push_back(u(e));
+using std::endl;
 
+vector<unsigned> random_binary_poly (const int n,const int w){
+  static std::default_random_engine e;
+  e.seed(std::chrono::system_clock::now().time_since_epoch().count());
+  static std::uniform_int_distribution<unsigned> u(0,n-1);
+  vector<unsigned> index;
+  vector<unsigned> ret;
+  unsigned temp;
+  bool bool_temp=true;
+
+  //randomly generate w positions where the value is 1
+
+  for(size_t i=0;i<w;++i){
+    temp=u(e);
+    //check if there are two same positions generated
+    for(auto it=index.begin();it!=index.end();++it){
+      if(*it==temp){
+	bool_temp=false;
+      }
+    }
+
+    if(bool_temp==true){
+      index.push_back(temp);
+    }
+    else {
+      --i;
+      bool_temp=true;
+    }
+  }
+  bool_temp=true;
+  for(int i=0;i<n;++i){
+    for (auto it=index.begin();it!=index.end();++it)
+      if (*it==i){
+	bool_temp=false;
+
+      }
+    if(bool_temp==false){
+      ret.push_back(1);
+      bool_temp=true;
+  
+    }
+    else{
+      ret.push_back(0);
+    }
   }
   return ret;
 }
 
 
 int main(int argc, char **argv){
-vector<int> check_poly_a;
-vector<int> check_poly_b;
+  const int n1=6;
+  const int n2=6;
+  const int w1=4;
+  const int w2=4;
+
+
+  vector<unsigned> check_poly_1=random_binary_poly(n1,w1);
+  vector<unsigned> check_poly_2=random_binary_poly(n2,w2);
+
+  for (int i=0;i<10;++i){
+    for (auto it=check_poly_1.begin();it!=check_poly_1.end();++it){
+      cout<<*it;
+
+    }  
+    cout<<endl;
+    check_poly_1=random_binary_poly(n1,w1);
+}
  cout<<"test";
 
 return 0;
