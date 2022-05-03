668388bc13a8e8497b042b6a7baf757179258d4d
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-05-03 13:16:45 -0700

    Delete n288_script.py

--- BP/n288_script.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("rm n288_data.txt")
-a=range(1,11)
-for n in a:
-    command4="./qBP toric_Hx_n12_cyc11 toric_Hz_n12_cyc11 %f 1000 20 n288_data.txt"%(0.005*n)
-    os.system(command4)
