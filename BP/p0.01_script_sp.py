6b5292c2ba7c9efe5ec4f874ad112e70d4bc37f0
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-05-03 13:17:11 -0700

    Delete p0.01_script_sp.py

--- BP/p0.01_script_sp.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("rm p0.01sp_data.txt")
-a=range(3,18)
-for n in a:
-    command4="./qBP toric_Hx_n%d_cyc11 toric_Hz_n%d_cyc11 0.01 2000 20 p0.01sp_data.txt"%(n,n)
-    os.system(command4)
