3a160c03aa0ed2e4528db0e9f527de3be08b19c6
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-05-03 12:31:55 -0700

    Add files via upload

--- BP/n128_script.py
+++ BP/n128_script.py
@@ -1,6 +1,6 @@
 import os
 os.system("rm n128_data.txt")
-a=range(1,11)
+a=range(1,15)
 for n in a:
-    command4="./qBP toric_Hx_n8_cyc11 toric_Hz_n8_cyc11 %f 1000 20 n128_data.txt 1"%(0.005*n)
+    command4="./qBP toric_Hx_n8_cyc11 toric_Hz_n8_cyc11 %f 2000 20 n128_data.txt 1"%(0.005*n)
     os.system(command4)
--- BP/p0.01_script.py
+++ BP/p0.01_script.py
@@ -1,6 +1,6 @@
 import os
-os.system("rm p0.01_data.txt")
-a=range(3,10)
+#os.system("rm p0.01_data.txt")
+a=range(14,18)
 for n in a:
-    command4="./qBP toric_Hx_n12_cyc11 toric_Hz_n12_cyc11 %f 100 20 n12_data_sp.txt"%(0.001*n)
+    command4="./qBP toric_Hx_n%d_cyc11 toric_Hz_n%d_cyc11 0.01 2000 20 p0.01_data.txt 1"%(n,n)
     os.system(command4)
