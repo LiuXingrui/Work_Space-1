ed00ec70107dd76df91ce2415bba827bcef4d9ec
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-05-16 17:04:36 -0700

    Add files via upload

--- BP/wt_n72.py
+++ BP/wt_n72.py
@@ -3,8 +3,8 @@ os.system("rm wt_n72d.txt")
 os.system("rm wt_n72s.txt")
 a=range(1,7)
 for n in a:
-    command="./qBP toric_Hx_n6_cyc11 toric_Hz_n6_cyc11 %f 10 1000 20 wt_n72d.txt "%n
+    command="./qBP toric_Hx_n6_cyc11 toric_Hz_n6_cyc11 %f 10 1000 20 wt_n72s.txt "%n
 
     os.system(command)
-    command="./qBP toric_Hx_n6_cyc11 toric_Hz_n6_cyc11 %f -1 1000 20 wt_n72s.txt "%n
+    command="./qBP toric_Hx_n6_cyc11 toric_Hz_n6_cyc11 %f -1 1000 20 wt_n72d.txt "%n
     os.system(command)
