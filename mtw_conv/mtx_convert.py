8363fe06411c4ef4a019c4ca64e2eaffef15c90d
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-05-17 20:36:55 -0700

    Add files via upload

--- mtw_conv/mtx_convert.py
+++ mtw_conv/mtx_convert.py
@@ -5,14 +5,31 @@ os.system("./conv.out GB_10_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4
 os.system("./conv.out GB_58_w4_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_2_X ")
 os.system("./conv.out GB_58_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_2_Z ")
 
-os.system("./conv.out GB_166_w4_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_3_X ")
-os.system("./conv.out GB_166_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_3_Z ")
+os.system("./conv.out GB_106_w4_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_3_X ")
+os.system("./conv.out GB_106_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_3_Z ")
 
-os.system("./conv.out GB_166_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_1_X ")
-os.system("./conv.out GB_166_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_1_Z ")
+os.system("./conv.out GB_166_w4_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_4_X ")
+os.system("./conv.out GB_166_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_4_Z ")
+
+os.system("./conv.out GB_202_w4_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_5_X ")
+os.system("./conv.out GB_202_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w4_5_Z ")
+
+
  
-os.system("./conv.out GB_10_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_2_X ")
-os.system("./conv.out GB_10_w4_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_2_Z ")
+os.system("./conv.out GB_10_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_1_X ")
+os.system("./conv.out GB_10_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_1_Z ")
 
-os.system("./conv.out GB_58_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_3_X ")
-os.system("./conv.out GB_58_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_3_Z ")
+os.system("./conv.out GB_58_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_2_X ")
+os.system("./conv.out GB_58_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_2_Z ")
+
+os.system("./conv.out GB_106_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_3_X ")
+os.system("./conv.out GB_106_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_3_Z ")
+
+os.system("./conv.out GB_166_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_4_X ")
+os.system("./conv.out GB_166_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_4_Z ")
+
+os.system("./conv.out GB_166_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_4_X ")
+os.system("./conv.out GB_166_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_4_Z ")
+
+os.system("./conv.out GB_202_w6_X.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_5_X ")
+os.system("./conv.out GB_202_w6_Z.mtx  ../../../Documents/Work_Space-1/S_BP/GB_w6_5_Z ")
