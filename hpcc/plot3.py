4a64919c5d30dbb193052f0aa0759a2c3c92c64b
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-06-14 16:04:50 -0700

    Update plot3.py

--- hpcc/plot3.py
+++ hpcc/plot3.py
@@ -12,7 +12,9 @@ for i in range(1,6):
     
     for p in pavg_list:
         for r in range_list:
+            # serial
             
-            command="./xqBP GB_w4_%d_X   GB_w4_%d_Z  %f %f 20000 100  bicycle_n%d_p%.3f_range%.1f.data 1 0 1 %f %f"%(i,i,p,r,n,p,r,p,r)
+            command="./xqBP forcopy/test/GB_w4_%d_X   forcopy/test/GB_w4_%d_Z  %f %f 20000 100  bicycle_w4_n%d_p%.3f_range%.1f.data 1 0 1 %f %f"%(i,i,p,r,n,p,r,p,r)
             os.system(command) 
-      
+            command="./xqBP forcopy/test/GB_w6_%d_X   forcopy/test/GB_w6_%d_Z  %f %f 20000 100  bicycle_w6_n%d_p%.3f_range%.1f.data 1 0 1 %f %f"%(i,i,p,r,n,p,r,p,r)
+            os.system(command) 
